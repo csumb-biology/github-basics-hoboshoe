b74578a69c281f6fc8e52984a600297264cb15f9
U-CSUMB\cart8745 <cart8745@53S-119-25720.csumb.ad.csumb.edu>
2019-04-02 18:39:22 -0700

    this is **a lot** edited already, oops

---
+++ human_subsample_1/hydropathy_graph.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+#Working template of hydropathy score calculation script
+#You need to put in comments for every line
+import sys
+
+InFileName = "amino_acid_hydropathy_values.txt"
+InFile = open(InFileName, 'r')
+Data=[]
+Hydropathy={}
+LineNumber = 0
+
+for i in range(len(sys.argv)):
+    if (i==1):
+        InSeqFileName=sys.argv[i]
+    else:
+        window=sys.argv[i]
+for Line in InFile:
+    if(LineNumber>0):
+        Line = Line.strip("\n")
+        Data = Line.split(",")
+        Hydropathy[Data[1]]=float(Data[2])
+    LineNumber = LineNumber + 1
+InFile.close()
+InSeqFile = open(InSeqFileName, 'r')
+LineNumber = 0
+
+for Line in InSeqFile:
+    if(LineNumber>0):
+        ProtSeq=Line.strip('\n')
+    LineNumber = LineNumber + 1
+InSeqFile.close()
+
+OutFileName = InSeqFileName.strip('.fasta') + ".output.csv"
+OutFile = open(OutFileName,"w")
+lets=0
+for win in range(len(ProtSeq)):
+    num=0
+    lets=sequence[win:win+window]
+    if(len(lets)==window):
+        for AA in lets:
+            num = float(hydrophobicity[AA][1])+num
+        OutString = "%d,%.2f" % (win, num)
+        OutFile.write(OutString + "\n")
+        OutFile.close()
